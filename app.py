36b7dc2544c8f552f296ad92647efc7868e47e4f
hanul choe <hanul.developer@gmail.com>
2022-11-17 14:54:02 +0900

    fix for merge2

--- app.py
+++ app.py
@@ -9,11 +9,3 @@ from pymongo import MongoClient
 ca = certifi.where()
 client = MongoClient("mongodb+srv://jominuk:alsdnr1549-@cluster0.pto4r2x.mongodb.net/?retryWrites=true&w=majority")
 db = client.sparta
-
-@app.route('/')
-def home():
-    return render_template('main.html')
-
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)

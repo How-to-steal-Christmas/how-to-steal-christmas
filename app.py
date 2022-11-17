f1987bec18388ed9152b286029e78a6f0b04699f
hanul choe <hanul.developer@gmail.com>
2022-11-17 14:45:46 +0900

    fix for merge

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
+import certifi
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
-
+import jwt
+import datetime
+import hashlib
 app = Flask(__name__)
 
 from pymongo import MongoClient
-
+ca = certifi.where()
 client = MongoClient("mongodb+srv://jominuk:alsdnr1549-@cluster0.pto4r2x.mongodb.net/?retryWrites=true&w=majority")
 db = client.sparta
 
-
 @app.route('/')
 def home():
-    return render_template('modal.html')
-
-
-@app.route("/modal", methods=["POST"])
-def modal_post():
-    url_receive = request.form['url_give']
-    coupon_receive = request.form['coupon_give']
-    comment_receive = request.form['comment_give']
-
-    doc = {
-        'url': url_receive,
-        'coupon': coupon_receive,
-        'comment': comment_receive
-    }
-    db.modal.insert_one(doc)
-    return jsonify({'msg': '선물 등록!'})
+    return render_template('main.html')
 
 
 if __name__ == '__main__':

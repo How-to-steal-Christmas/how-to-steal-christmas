1f1cb6375c98e0cff3a7a1def45713d4f70aa004
hanul choe <hanul.developer@gmail.com>
2022-11-17 19:26:35 +0900

    merge branches

--- app.py
+++ app.py
@@ -15,21 +15,9 @@ SECRET_KEY = 'tree'
 def home():
     return render_template("home.html")
 
-@app.route('/main')
-def main():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.user.find_one({"id": payload['id']})
-
-        return render_template('index.html')
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
 def login():
    return render_template('login.html')
+
 @app.route('/login')
 def login():
    return render_template('login.html')
@@ -38,8 +26,29 @@ def login():
 def register():
    return render_template('register.html')
 
+@app.route('/treeforguest')
+def treeforguest():
+   return render_template('treeforguest.html')
+
+@app.route('/treeforuser')
+def treeforuser():
+   return render_template('treeforuser.html')
+
+@app.route('/main')
+def main():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"id": payload['id']})
+
+        return render_template('mycalendars.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
 #################################
-##  로그인을 위한 API             ##
+##    Login , Register API     ##
 #################################
 @app.route('/user/login', methods=["POST"])
 def user_login():
@@ -56,11 +65,9 @@ def user_login():
 
         payload = {
             'id': user_email,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=1005)
         }
-        print(payload)
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-        print(token)
         return jsonify({'result': 'success', 'token': token})
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 존재하지 않습니다.'})
@@ -77,21 +84,156 @@ def user_register():
 
     return jsonify({'result': 'success'})
 
+#################################
+## createtree, mycalendar API  ##
+#################################
+@app.route("/createtree", methods=["POST"])
+def create_tree():
+    calendar_list = list(db.calendar.find({}, {'_id': False}))
+    count = len(calendar_list)
+
+    today_receive = request.form['today_give']
+    email_receive = request.form['email_give']
+    user_name_receive = request.form['user_name_give']
+    calendar_receive = request.form['calendar_name_give']
+    file_receive = request.form['file_give']
+    url_receive = request.form['url_give']
+    coupon_receive = request.form['coupon_give']
+    comment_receive = request.form['comment_give']
+    create_time_receive = request.form['create_time_give']
+
+    print(create_time_receive, today_receive)
+
+    doc = {
+        'idx': count,
+        'today': today_receive,
+        'email': email_receive,
+        'user_name': user_name_receive,
+        'calendar_name': calendar_receive,
+        'create_time': create_time_receive,
+        'item': [
+            {
+                'date': '2022-11-14',
+                'file': file_receive,
+                'url': url_receive,
+                'coupon': coupon_receive,
+                'comment': comment_receive
+            },
+            {
+                'date': '2022-11-15',
+                'file': file_receive,
+                'url': url_receive,
+                'coupon': coupon_receive,
+                'comment': comment_receive
+            },
+            {
+                'date': '2022-11-16',
+                'file': file_receive,
+                'url': url_receive,
+                'coupon': coupon_receive,
+                'comment': comment_receive
+            },
+            {
+                'date': '2022-11-17',
+                'file': file_receive,
+                'url': url_receive,
+                'coupon': coupon_receive,
+                'comment': comment_receive
+            },
+            {
+                'date': '2022-11-18',
+                'file': file_receive,
+                'url': url_receive,
+                'coupon': coupon_receive,
+                'comment': comment_receive
+            },
+            {
+                'date': '2022-11-19',
+                'file': file_receive,
+                'url': url_receive,
+                'coupon': coupon_receive,
+                'comment': comment_receive
+            },
+            {
+                'date': '2022-11-20',
+                'file': file_receive,
+                'url': url_receive,
+                'coupon': coupon_receive,
+                'comment': comment_receive
+            }
+        ]
+    }
+
+    db.calendar.insert_one(doc)
+    return jsonify({'msg': '등록 완료!'})
+
+@app.route("/mycalendars", methods=["GET"])
+def calendar_get():
+    # email_receive = request.form['email']
+    calendar_list = list(db.calendar.find({}, {'_id': False}))
+    return jsonify({'calendar': calendar_list})
+
+@app.route("/mycalendars/delete", methods=["POST"])
+def calendar_delete():
+    idx_receive = request.form['idx_give']
+    mail_receive = request.form['mail_give']
+    user_name_receive = request.form['user_name_give']
+
+    db.calendar.delete_one({'idx': int(idx_receive)})
+    return jsonify({'msg': '삭제 완료!'})
+
+@app.route("/mycalendars/edit/calendar", methods=["POST"])
+def calendar_edit():
+    idx_receive = request.form['idx_give']
+    calendar_name_receive = request.form['calendar_name_give']
+
+    db.calendar.update_one({'idx': int(idx_receive)}, {'$set': {'calendar_name': calendar_name_receive}})
+    return jsonify({'msg': '수정 완료!'})
+
+#################################
+##         createcards API     ##
+#################################
+@app.route("/createcards", methods=["POST"])
+def cards_post():
+   url_receive = request.form['url_give']
+   coupon_receive = request.form['coupon_give']
+   comment_receive = request.form['comment_give']
+
+   doc = {
+      'url':url_receive,
+      'coupon':coupon_receive,
+      'comment':comment_receive
+   }
+   db.modal.insert_one(doc)
+   return jsonify({'msg':'선물 등록!'})
+
+#################################
+##           tree API          ##
+#################################
+@app.route('/tree/details', methods=['GET'])
+def modal_data_get():
+    calendar = request.args.get('calendar_name')
+    print('calendar', calendar)
+    modals = db.calendar.find_one({'calendar_name': calendar}, {'_id':False})
+    print(modals)
+    return jsonify({'modals': modals})
+
+#################################
+##    createcards API          ##
+#################################
+@app.route("/createcards/detail", methods=["POST"])
+def modal_post():
+   url_receive = request.form['url_give']
+   coupon_receive = request.form['coupon_give']
+   comment_receive = request.form['comment_give']
+   calendar_receive = request.form['calendar_name']
+
+   idx_receive = request.form['idx']
+    
+   # db.calendar.update_one({'calendar_name': calendar_receive}, {'$set': 'item'[idx_receive][{'url': url_receive, 'coupon': coupon_receive, 'comment': comment_receive}]})
+
+   return jsonify({'msg':'선물 등록!'})
+
+
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
-
-# 저장 - 예시
-# doc = {'name':'bobby','age':21}
-# db.users.insert_one(doc)
-#
-# # 한 개 찾기 - 예시
-# user = db.users.find_one({'name':'bobby'})
-#
-# # 여러개 찾기 - 예시 ( _id 값은 제외하고 출력)
-# all_users = list(db.users.find({},{'_id':False}))
-#
-# # 바꾸기 - 예시
-# db.users.update_one({'name':'bobby'},{'$set':{'age':19}})
-#
-# # 지우기 - 예시
-# db.users.delete_one({'name':'bobby'})
